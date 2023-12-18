791cb97bff77afedad48d948ac991f259a66baf6
Lucas Mariano Grigolato <lgrigolato@emtech.com.ar>
2023-12-18 21:43:27 +0100

    Add compile-time implementation of clog2

--- cpp/fft_sysdef.h
+++ cpp/fft_sysdef.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "ap_fixed.h"
 
+// Constexpr function to calculate the ceiling of the base-2 logarithm using recursion
+constexpr unsigned int clog2_recursive(unsigned int n, unsigned int p = 0)
+{
+    return (n <= 1) ? p : clog2_recursive((n + 1) / 2, p + 1);
+}
+
 // Constants
 static const int N = 256;
-static const int n_clog2_c = 8;
+static const int n_clog2_c = clog2_recursive(N);
 
 // Types
 typedef ap_int<24> TI_INPUT_SIGNAL;
