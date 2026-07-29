215cae9472e904e562255d52ef6e517637157fcb
diego <diego@cholulaelectronics.studio>
2026-07-29 17:06:03 +0200

    Add axi dma to zybo project

--- cfg/fft_demo_integ/bd_project.tcl
+++ cfg/fft_demo_integ/bd_project.tcl
@@ -130,6 +130,7 @@ if { $bCheckIPs == 1 } {
    set list_check_ips "\ 
 xilinx.com:ip:axi_gpio:2.0\
 xilinx.com:hls:fft_wrapper:1.0\
+xilinx.com:ip:axi_dma:7.1\
 fft_demo:user:codec_unit_top:1.0\
 xilinx.com:ip:xlconstant:1.1\
 xilinx.com:ip:processing_system7:5.5\
@@ -208,6 +209,8 @@ proc create_hier_cell_zynq_cpu { parentCell nameHier } {
   create_bd_intf_pin -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 M02_AXI
 
   create_bd_intf_pin -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 M00_AXI
+
+  create_bd_intf_pin -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 M03_AXI
 
 
   # Create pins
@@ -488,7 +491,7 @@ proc create_hier_cell_zynq_cpu { parentCell nameHier } {
 
   # Create instance: axi_interconnect_zynq, and set properties
   set axi_interconnect_zynq [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect_zynq ]
-  set_property CONFIG.NUM_MI {3} $axi_interconnect_zynq
+  set_property CONFIG.NUM_MI {4} $axi_interconnect_zynq
 
 
   # Create instance: intr_concat_zynq, and set properties
@@ -504,6 +507,7 @@ proc create_hier_cell_zynq_cpu { parentCell nameHier } {
   connect_bd_intf_net -intf_net Conn2 [get_bd_intf_pins zynq/FIXED_IO] [get_bd_intf_pins FIXED_IO]
   connect_bd_intf_net -intf_net Conn3 [get_bd_intf_pins axi_interconnect_zynq/M02_AXI] [get_bd_intf_pins M02_AXI]
   connect_bd_intf_net -intf_net Conn4 [get_bd_intf_pins axi_interconnect_zynq/M00_AXI] [get_bd_intf_pins M00_AXI]
+  connect_bd_intf_net -intf_net Conn5 [get_bd_intf_pins axi_interconnect_zynq/M03_AXI] [get_bd_intf_pins M03_AXI]
   connect_bd_intf_net -intf_net S00_AXI_1 [get_bd_intf_pins axi_interconnect_zynq/S00_AXI] [get_bd_intf_pins zynq/M_AXI_GP0]
   connect_bd_intf_net -intf_net axi_interconnect_zynq_M01_AXI [get_bd_intf_pins M01_AXI] [get_bd_intf_pins axi_interconnect_zynq/M01_AXI]
 
@@ -519,7 +523,8 @@ proc create_hier_cell_zynq_cpu { parentCell nameHier } {
   [get_bd_pins axi_interconnect_zynq/M01_ARESETN] \
   [get_bd_pins axi_interconnect_zynq/M02_ARESETN] \
   [get_bd_pins axi_interconnect_zynq/S00_ARESETN] \
-  [get_bd_pins peripheral_aresetn]
+  [get_bd_pins peripheral_aresetn] \
+  [get_bd_pins axi_interconnect_zynq/M03_ARESETN]
   connect_bd_net -net intr_concat_zynq_dout  [get_bd_pins intr_concat_zynq/dout] \
   [get_bd_pins zynq/IRQ_F2P]
   connect_bd_net -net zynq_FCLK_CLK0  [get_bd_pins zynq/FCLK_CLK0] \
@@ -530,7 +535,8 @@ proc create_hier_cell_zynq_cpu { parentCell nameHier } {
   [get_bd_pins axi_interconnect_zynq/M02_ACLK] \
   [get_bd_pins axi_interconnect_zynq/S00_ACLK] \
   [get_bd_pins zynq/M_AXI_GP0_ACLK] \
-  [get_bd_pins cpu_reset_gen/slowest_sync_clk]
+  [get_bd_pins cpu_reset_gen/slowest_sync_clk] \
+  [get_bd_pins axi_interconnect_zynq/M03_ACLK]
   connect_bd_net -net zynq_FCLK_RESET0_N  [get_bd_pins zynq/FCLK_RESET0_N] \
   [get_bd_pins cpu_reset_gen/ext_reset_in]
 
@@ -693,17 +699,31 @@ proc create_root_design { parentCell } {
   # Create instance: fft_wrapper_0, and set properties
   set fft_wrapper_0 [ create_bd_cell -type ip -vlnv xilinx.com:hls:fft_wrapper:1.0 fft_wrapper_0 ]
 
+  # Create instance: fft_dma, and set properties
+  set fft_dma [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 fft_dma ]
+  set_property -dict [list \
+    CONFIG.c_include_mm2s {1} \
+    CONFIG.c_include_s2mm {1} \
+    CONFIG.c_include_sg {0} \
+    CONFIG.c_m_axi_s2mm_data_width {64} \
+    CONFIG.c_s_axis_s2mm_tdata_width {64} \
+    CONFIG.c_sg_length_width {26} \
+  ] $fft_dma
+
+
   # Create interface connections
   connect_bd_intf_net -intf_net axi_gpio_0_GPIO [get_bd_intf_ports SW] [get_bd_intf_pins axi_gpio_0/GPIO]
   connect_bd_intf_net -intf_net axi_gpio_0_GPIO2 [get_bd_intf_ports BTN] [get_bd_intf_pins axi_gpio_0/GPIO2]
+  connect_bd_intf_net -intf_net fft_dma_M_AXIS_MM2S [get_bd_intf_pins fft_wrapper_0/input_signal_stream] [get_bd_intf_pins fft_dma/M_AXIS_MM2S]
+  connect_bd_intf_net -intf_net fft_wrapper_0_fft_output_stream [get_bd_intf_pins fft_dma/S_AXIS_S2MM] [get_bd_intf_pins fft_wrapper_0/fft_output_stream]
   connect_bd_intf_net -intf_net sampler_LED [get_bd_intf_ports LED] [get_bd_intf_pins sampler/LED]
-  connect_bd_intf_net -intf_net sampler_axis_interface_master [get_bd_intf_pins sampler/axis_interface_master] [get_bd_intf_pins fft_wrapper_0/input_signal_stream]
   connect_bd_intf_net -intf_net sampler_codec_i2s [get_bd_intf_ports codec_i2s] [get_bd_intf_pins sampler/codec_i2s]
   connect_bd_intf_net -intf_net zynq_cpu_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins zynq_cpu/DDR]
   connect_bd_intf_net -intf_net zynq_cpu_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins zynq_cpu/FIXED_IO]
   connect_bd_intf_net -intf_net zynq_cpu_M00_AXI [get_bd_intf_pins zynq_cpu/M00_AXI] [get_bd_intf_pins fft_wrapper_0/s_axi_axi4l_if]
   connect_bd_intf_net -intf_net zynq_cpu_M01_AXI [get_bd_intf_pins zynq_cpu/M01_AXI] [get_bd_intf_pins sampler/axi4_lite_interface]
   connect_bd_intf_net -intf_net zynq_cpu_M02_AXI [get_bd_intf_pins zynq_cpu/M02_AXI] [get_bd_intf_pins axi_gpio_0/S_AXI]
+  connect_bd_intf_net -intf_net zynq_cpu_M03_AXI [get_bd_intf_pins fft_dma/S_AXI_LITE] [get_bd_intf_pins zynq_cpu/M03_AXI]
 
   # Create port connections
   connect_bd_net -net Net  [get_bd_ports i2c_scl] \
@@ -720,23 +740,27 @@ proc create_root_design { parentCell } {
   [get_bd_pins axi_gpio_0/s_axi_aclk] \
   [get_bd_pins fft_wrapper_0/clk] \
   [get_bd_pins sampler/axi_clk] \
-  [get_bd_pins fft_wrapper_0/ap_clk]
+  [get_bd_pins fft_wrapper_0/ap_clk] \
+  [get_bd_pins fft_dma/s_axi_lite_aclk] \
+  [get_bd_pins fft_dma/m_axi_mm2s_aclk] \
+  [get_bd_pins fft_dma/m_axi_s2mm_aclk]
   connect_bd_net -net zynq_cpu_peripheral_aresetn  [get_bd_pins zynq_cpu/peripheral_aresetn] \
   [get_bd_pins axi_gpio_0/s_axi_aresetn] \
   [get_bd_pins fft_wrapper_0/ap_rst_n_clk] \
   [get_bd_pins fft_wrapper_0/ap_rst_n] \
-  [get_bd_pins sampler/s00_axi_aresetn]
+  [get_bd_pins sampler/s00_axi_aresetn] \
+  [get_bd_pins fft_dma/axi_resetn]
 
   # Create address segments
   assign_bd_address -offset 0x41200000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_cpu/zynq/Data] [get_bd_addr_segs axi_gpio_0/S_AXI/Reg] -force
   assign_bd_address -offset 0x43C00000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_cpu/zynq/Data] [get_bd_addr_segs sampler/codec_controller/axi4_lite_interface_memory_map/codec_controller_regmap] -force
+  assign_bd_address -offset 0x40400000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_cpu/zynq/Data] [get_bd_addr_segs fft_dma/S_AXI_LITE/Reg] -force
   assign_bd_address -offset 0x40000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_cpu/zynq/Data] [get_bd_addr_segs fft_wrapper_0/s_axi_axi4l_if/Reg] -force
 
 
   # Restore current instance
   current_bd_instance $oldCurInst
 
-  validate_bd_design
   save_bd_design
 }
 # End of create_root_design()
@@ -748,4 +772,6 @@ proc create_root_design { parentCell } {
 
 create_root_design ""
 
+
+common::send_gid_msg -ssname BD::TCL -id 2053 -severity "WARNING" "This Tcl script was generated from a block design that has not been validated. It is possible that design <$design_name> may result in errors during validation."
 
